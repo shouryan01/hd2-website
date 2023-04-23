99e1b7706e98848e6fea5548658a1c92e097013c
Rohit Nair <6954311+rnair98@users.noreply.github.com>
2023-04-23 00:52:36 -0400

    update: rain.tsx

--- components/Rain.tsx
+++ components/Rain.tsx
@@ -113,11 +113,12 @@ const Rain: React.FC = () => {
           const isFirst = index === 0;
           const lightness = isFirst ? 255 : 200;
       
-          this.p.fill(...this.color, lightness);
+          this.p.fill(this.color[0], this.color[1], this.color[2], lightness);
           this.p.text(symbol.value, symbol.x, symbol.y);
           symbol.rain();
           symbol.setToRandomSymbol();
         });
+      
       }
       
     }
