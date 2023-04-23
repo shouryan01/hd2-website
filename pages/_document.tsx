6c5703b1feb93c18db4162c80677419eaca93f22
Rohit Nair <6954311+rnair98@users.noreply.github.com>
2023-04-22 20:24:14 -0400

    update: linting and formatting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@700&family=IBM+Plex+Sans:wght@400;700&family=Rubik+Glitch&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@700&family=IBM+Plex+Sans:wght@400;700&family=Rubik+Glitch&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,35 @@ import React from "react";
 import Image from "next/image";
 import disrupt from "../public/disrupt.svg";
 
-function NavBar({ pages }) {
-  return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        {pages.map((page, index) => (
-          <li key={index} className="nav-item">
-            <a href="#" className="nav-link" data-text={page}>
-              {page}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+function NavBar({ pages }: { pages: string[] }) {
+	return (
+		<nav className="navbar">
+			<ul className="nav-list">
+				{pages.map((page, index) => (
+					<li key={index} className="nav-item">
+						<a href="#" className="nav-link" data-text={page}>
+							{page}
+						</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 }
 
 export default function Home() {
-  const pages = ["register", "about", "sponsors", "prizes", "faq", "contact"];
+	const pages = ["register", "about", "sponsors", "prizes", "faq", "contact"];
 
-  return (
-    <>
-      <main className="flex min-h-screen flex-col items-center p-24 main-container">
-        <h1 className="d3">hack dearborn 2</h1>
-        <div className="relative">
-          <h3 className="d4">Disrupt Reality</h3>
-          <h5 className="d5">10.22.2023</h5>
-        </div>
-        <NavBar pages={pages} />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<main className="flex min-h-screen flex-col items-center p-24 main-container">
+				<h1 className="d3">hack dearborn 2</h1>
+				<div className="relative">
+					<h3 className="d4">Disrupt Reality</h3>
+					<h5 className="d5">10.22.2023</h5>
+				</div>
+				<NavBar pages={pages} />
+			</main>
+		</>
+	);
 }
