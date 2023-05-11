433edade9312f8c582aa812ef63577c95a5a53ea
Shashanka Prajapati <shashankap95@gmail.com>
2023-05-11 10:21:45 -0400

    [#36] [feat] Add the sponsors page with a header

---
+++ pages/sponsors.tsx
@@ -0,0 +1,14 @@
+const Sponsors = () => {
+  return (
+    <div className="w-full px-10 pt-20 md:px-20 lg:px-40 lg:pb-20">
+      <div>
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
+          Sponsors
+        </h1>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Sponsors
