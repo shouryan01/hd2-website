c3b49b2fa53c5c582502bf6a99454956f9b0bd79
zahraa222 <zzmmk2003@gmail.com>
2023-05-15 00:24:09 -0400

    disableScroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Grid from "../components/grid";
 
 const Home: React.FC = () => {
   const [darkMode, setDarkMode] = useState(true);
-
+  const [scrollDisabled, setScrollDisabled] = useState(true);
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
@@ -20,8 +20,9 @@ const Home: React.FC = () => {
   ];
 
   useEffect(() => {
+	document.body.style.overflow = scrollDisabled ? "hidden" : "auto";
     const handleScroll = () => {};
-
+	
     Events.scrollEvent.register("begin", handleScroll);
     Events.scrollEvent.register("end", handleScroll);
     window.addEventListener("scroll", handleScroll);
@@ -31,22 +32,27 @@ const Home: React.FC = () => {
       Events.scrollEvent.remove("end");
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+	
+  }, [scrollDisabled]);
 
   return (
     <>
-	<div className="fixed top-50 left-5 p-4 flex items-center justify-center h-10 w-10 bg-red-500">
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM17.78 9.22a.75.75 0 10-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 001.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 101.06-1.06L20.56 12l1.72-1.72a.75.75 0 00-1.06-1.06l-1.72 1.72-1.72-1.72z" />
-            </svg>
-          </button>
-        </div>
+	
+      <div className="fixed top-50 left-5 p-4 flex items-center justify-center h-10 w-10 bg-red-500">
+        <button  onClick={async () => {
+			setScrollDisabled(false); //enable scrolling
+		}}>
+		
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM17.78 9.22a.75.75 0 10-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 001.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 101.06-1.06L20.56 12l1.72-1.72a.75.75 0 00-1.06-1.06l-1.72 1.72-1.72-1.72z" />
+          </svg>
+        </button>
+      </div>
       <div
         className={`min-h-screen transition-colors duration-500 ${
           darkMode ? "bg-dark-background text-white" : "bg-white text-black"
