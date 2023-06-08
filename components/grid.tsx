443504f62b14f18d2073977d662008342629413b
Shashanka Prajapati <shashankap95@gmail.com>
2023-06-08 18:51:50 +0545

    [#58] [feat] Embed typeform

--- components/grid.tsx
+++ components/grid.tsx
@@ -1,30 +1,44 @@
 // Desc: Grid component for displaying pages
-import Link from 'next/link';
+import Link from "next/link";
+
+import { SliderButton } from "@typeform/embed-react";
 
 type Page = {
 	title: string;
 	slug: string;
-}
+};
 
-const Grid = ({pages}:{pages: Array<Page>}) => {
+const Grid = ({ pages }: { pages: Array<Page> }) => {
 	return (
-	<div className="container mx-auto p-4 flex justify-center">
-		<div className="w-2/3 grid grid-cols-2 md:grid-cols-3 gap-6">
-			{pages.map((page,index) => (
-				<Link
-					key={index}
-					href={`${page.slug}`}
-					className="p-6 border-2 border-white rounded-lg dark:border-white hover:bg-blue-custom hover:border-blue-custom transition-all"
-				>
-					<div className="flex justify-between items-center">
-						<h4 className="text-xl font-bold mb-2">{page.title}</h4>
-						<span className="text-xl font-bold mb-2">➤</span>
-					</div>
-				</Link>
-			))}
+		<div className="container mx-auto p-4 flex justify-center">
+			<div className="w-2/3 grid grid-cols-2 md:grid-cols-3 gap-6">
+				{pages.map((page, index) =>
+					page.slug === "register" ? (
+						<SliderButton
+							id={"oNcQNjvg"}
+							className="p-6 border-2 border-white rounded-lg dark:border-white hover:bg-blue-custom hover:border-blue-custom transition-all"
+						>
+							<div className="flex justify-between items-center">
+								<h4 className="text-xl font-bold mb-2">{page.title}</h4>
+								<span className="text-xl font-bold mb-2">➤</span>
+							</div>
+						</SliderButton>
+					) : (
+						<Link
+							key={index}
+							href={`${page.slug}`}
+							className="p-6 border-2 border-white rounded-lg dark:border-white hover:bg-blue-custom hover:border-blue-custom transition-all"
+						>
+							<div className="flex justify-between items-center">
+								<h4 className="text-xl font-bold mb-2">{page.title}</h4>
+								<span className="text-xl font-bold mb-2">➤</span>
+							</div>
+						</Link>
+					),
+				)}
+			</div>
 		</div>
-	</div>
 	);
-}
+};
 
 export default Grid;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,45 @@
-import React, { useEffect, useState } from "react";
-
-import { Events } from "react-scroll";
-import Grid from "../components/grid";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Events } from "react-scroll";
+
+import Grid from "@/components/grid";
 
 const Home: React.FC = () => {
-  const [darkMode, setDarkMode] = useState(true);
-  const [scrollDisabled, setScrollDisabled] = useState(false);
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+	const [darkMode, setDarkMode] = useState(true);
+	const [scrollDisabled, setScrollDisabled] = useState(false);
+	const toggleDarkMode = () => {
+		setDarkMode(!darkMode);
+	};
 
-  const pages = [
-    { title: "Apply", slug: "register" },
-    { title: "About", slug: "about" },
-    { title: "Sponsors", slug: "sponsors" },
-    { title: "Prizes", slug: "prizes" },
-    { title: "FAQ", slug: "faq" },
-    { title: "Contact", slug: "contact" },
-    { title: "Our Team", slug: "ourteam" },
-  ];
+	const pages = [
+		{ title: "Register", slug: "register" },
+		{ title: "About", slug: "about" },
+		{ title: "Sponsors", slug: "sponsors" },
+		{ title: "Prizes", slug: "prizes" },
+		{ title: "FAQ", slug: "faq" },
+		{ title: "Contact", slug: "contact" },
+		{ title: "Our Team", slug: "ourteam" },
+	];
 
-  // useEffect(() => {
-  //   document.body.style.overflow = scrollDisabled ? "auto" : "hidden";
-  //   const handleScroll = () => { };
+	// useEffect(() => {
+	//   document.body.style.overflow = scrollDisabled ? "auto" : "hidden";
+	//   const handleScroll = () => { };
 
-  //   Events.scrollEvent.register("begin", handleScroll);
-  //   Events.scrollEvent.register("end", handleScroll);
-  //   window.addEventListener("scroll", handleScroll);
+	//   Events.scrollEvent.register("begin", handleScroll);
+	//   Events.scrollEvent.register("end", handleScroll);
+	//   window.addEventListener("scroll", handleScroll);
 
-  //   return () => {
-  //     Events.scrollEvent.remove("begin");
-  //     Events.scrollEvent.remove("end");
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
+	//   return () => {
+	//     Events.scrollEvent.remove("begin");
+	//     Events.scrollEvent.remove("end");
+	//     window.removeEventListener("scroll", handleScroll);
+	//   };
 
-  // }, [scrollDisabled]);
+	// }, [scrollDisabled]);
 
-  return (
-    <>
-
-      {/* <div className="fixed top-50 left-5 p-4 flex items-center justify-center h-10 w-10 bg-red-500">
+	return (
+		<>
+			{/* <div className="fixed top-50 left-5 p-4 flex items-center justify-center h-10 w-10 bg-red-500">
         <button  onClick={async () => {
 			setScrollDisabled(!scrollDisabled); //enable scrolling
 		}}>
@@ -55,42 +54,43 @@ const Home: React.FC = () => {
           </svg>
         </button>
       </div> */}
-      <div
-        className={`min-h-screen transition-colors duration-500 ${darkMode ? "bg-dark-background text-white" : "bg-white text-black"
-          }`}
-      >
-        <header className="container mx-auto mb-0 flex justify-center items-center">
-          <div className="logo">
-            <Image
-              src="/hd_logo.jpeg"
-              width={500}
-              height={500}
-              alt="hackathon logo"
-            />
-          </div>
-        </header>
-        <main className="container mx-auto -mt-8 px-[22px]">
-          <div className="text-center">
-            <h1 className="date">10.22.2023</h1>
-          </div>
-          <Grid pages={pages} />
-        </main>
-        <a
-          id="mlh-trust-badge"
-          className="block fixed right-12 top-0 w-20 z-40"
-          href="https://mlh.io/na?utm_source=na-hackathon&amp;utm_medium=TrustBadge&amp;utm_campaign=2023-season&amp;utm_content=black"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-black.svg"
-            alt="Major League Hacking 2023 Hackathon Season"
-            className="w-full"
-          />
-        </a>
-      </div>
-    </>
-  );
+			<div
+				className={`min-h-screen transition-colors duration-500 ${
+					darkMode ? "bg-dark-background text-white" : "bg-white text-black"
+				}`}
+			>
+				<header className="container mx-auto mb-0 flex items-center justify-center">
+					<div className="logo">
+						<Image
+							src="/hd_logo.jpeg"
+							width={500}
+							height={500}
+							alt="hackathon logo"
+						/>
+					</div>
+				</header>
+				<main className="container mx-auto -mt-8 px-[22px]">
+					<div className="text-center">
+						<h1 className="date">10.22.2023</h1>
+					</div>
+					<Grid pages={pages} />
+				</main>
+				<a
+					id="mlh-trust-badge"
+					className="fixed right-12 top-0 z-40 block w-20"
+					href="https://mlh.io/na?utm_source=na-hackathon&amp;utm_medium=TrustBadge&amp;utm_campaign=2023-season&amp;utm_content=black"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-black.svg"
+						alt="Major League Hacking 2023 Hackathon Season"
+						className="w-full"
+					/>
+				</a>
+			</div>
+		</>
+	);
 };
 
 export default Home;
