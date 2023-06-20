6d93dc4605a822a8d6e862d68b47198c9acb07d8
Shouryan Nikam <snnikam@umich.edu>
2023-06-20 11:25:44 -0400

    normalize image sizes

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,47 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
-const Gallery = ({ images }) => {
+type ImageType = {
+  url: string;
+  name: string;
+  link: string;
+};
+
+const Gallery = ({ images }: any) => {
   return (
     <div className="grid grid-cols-5 gap-4">
-      {images.map((image, index) => (
+      {images.map((image: ImageType, index: number) => (
         <div key={index} className="text-center">
-          <div className="glitcheffect">
-            <img
-              src={image.url}
-              alt={image.name}
-              onMouseOver={() =>
-                document
-                  .getElementById(`glitch-${index}`)
-                  .classList.add("hover-glitch")
-              }
-              onMouseOut={() =>
-                document
-                  .getElementById(`glitch-${index}`)
-                  .classList.remove("hover-glitch")
-              }
-            />
-            <div className="glitchlayers" id={`glitch-${index}`}>
-              <div
-                className="glitch"
-                style={{ backgroundImage: `url(${image.url})` }}
-              />
-              <div
-                className="glitch"
-                style={{ backgroundImage: `url(${image.url})` }}
-              />
-              <div
-                className="glitch"
-                style={{ backgroundImage: `url(${image.url})` }}
-              />
-            </div>
-          </div>
+          <img
+            src={image.url}
+            alt={image.name}
+            style={{ width: "100%", height: "20vw" }}
+            className="rounded-xl"
+          />
           <p>{image.name}</p>
           <button className="w-10 h-10">
             <a href={image.link} target="_blank" rel="noopener noreferrer">
-              <img
-                src="https://static.vecteezy.com/system/resources/previews/018/930/587/non_2x/linkedin-logo-linkedin-icon-transparent-free-png.png"
+              <Image
+                src="/linkedin-logo.png"
                 alt="LinkedIn  Profile"
                 className="w-full h-full"
+                width={100}
+                height={100}
               />
             </a>
           </button>
