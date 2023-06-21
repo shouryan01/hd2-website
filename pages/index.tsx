c1d1ff142e2a8801360105b1466b3977388a2fce
Shouryan Nikam <snnikam@umich.edu>
2023-06-20 20:51:46 -0400

    rest

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,104 +110,106 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 700,
-            easing: "ease-in",
-            properties: [
-              {
-                startValue: 1,
-                endValue: 1.6,
-                property: "scale"
-              }
-            ]
-          }
-        ]}
-        style={{
-          position: "fixed",
-          left: 0,
-          top: 0,
-          width: "100%",
-          zIndex: 100
-        }}
-      >
-        <img style={{ width: "100%" }} src="bg.png" alt="foreground" />
-      </Plx>
-      <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 800,
-            properties: [
-              {
-                startValue: 1,
-                endValue: 1.18,
-                property: "scale"
-              }
-            ]
-          }
-        ]}
-        style={{
-          position: "fixed",
-          left: 0,
-          top: 0,
-          width: "100%"
-        }}
-      >
-        <img style={{ width: "100%" }} src="background.jpg" alt="background" />
-      </Plx>
-      <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 400,
-            properties: [
-              {
-                startValue: 1,
-                endValue: 0,
-                property: "opacity"
-              }
-            ]
-          }
-        ]}
-        style={{
-          position: "fixed",
-          left: 0,
-          top: "26vw",
-          width: "100%"
-        }}
-      >
-        <img
+    <>
+      <div>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 700,
+              properties: [
+                {
+                  startValue: 1,
+                  endValue: 1.6,
+                  property: "scale"
+                }
+              ]
+            }
+          ]}
           style={{
-            width: "30vw"
+            position: "fixed",
+            left: 0,
+            top: 0,
+            width: "100%",
+            zIndex: 100
           }}
-          src="/text-img.webp"
-          alt="Goonies"
-        />
-      </Plx>
-      <div
-        style={{
-          position: "fixed",
-          left: 0,
-          top: 0,
-          zIndex: 200,
-          paddingTop: "56%",
-          height: "400vh",
-          width: "100%"
-        }}
-      >
+        >
+          <img style={{ width: "100%" }} src="bg.png" alt="foreground" />
+        </Plx>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 800,
+              properties: [
+                {
+                  startValue: 1,
+                  endValue: 1.18,
+                  property: "scale"
+                }
+              ]
+            }
+          ]}
+          style={{
+            position: "fixed",
+            left: 0,
+            top: 0,
+            width: "100%"
+          }}
+        >
+          <img style={{ width: "100%" }} src="background.jpg" alt="background" />
+        </Plx>
+        <Plx
+          parallaxData={[
+            {
+              start: 0,
+              end: 400,
+              properties: [
+                {
+                  startValue: 1,
+                  endValue: 0,
+                  property: "opacity"
+                }
+              ]
+            }
+          ]}
+          style={{
+            position: "fixed",
+            left: 0,
+            top: "26vw",
+            width: "100%"
+          }}
+        >
+          <img
+            style={{
+              width: "30vw"
+            }}
+            src="/text-img.webp"
+            alt="Goonies"
+          />
+        </Plx>
         <div
           style={{
-            background: "#000",
-            height: "100%"
+            position: "fixed",
+            left: 0,
+            top: 0,
+            zIndex: 200,
+            paddingTop: "56%",
+            height: "400vh",
+            width: "100%"
           }}
-        ></div>
-      </div>
-    </div>
+        >
+          <div
+            style={{
+              background: "#000",
+              height: "100%"
+            }}
+          ></div>
+        </div>
 
+        {/* <Sponsors /> */}
+      </div>
+    </>
     // <>
     //   <div
     //     className={backgroundImage}
