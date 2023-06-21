6198576fc8e231fde786e894f728d36bffdae932
Shouryan Nikam <snnikam@umich.edu>
2023-06-20 20:45:24 -0400

    more tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Faq from "@/components/Faq";
 import MLH_Trust from "../components/MLH_Trust";
 import MuteButton from "@/components/Mute_Button";
 import OurTeam from "@/components/ourteam";
+import Plx from "react-plx";
 import Prizes from "@/components/Prizes";
 import Sponsors from "@/components/Sponsors";
 import dynamic from "next/dynamic";
@@ -110,8 +111,103 @@ export default function Home() {
 
   return (
     <div>
-      hi
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 700,
+            easing: "ease-in",
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.6,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%",
+          zIndex: 100
+        }}
+      >
+        <img style={{ width: "100%" }} src="bg.png" alt="foreground" />
+      </Plx>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 800,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.18,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%"
+        }}
+      >
+        <img style={{ width: "100%" }} src="background.jpg" alt="background" />
+      </Plx>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 400,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 0,
+                property: "opacity"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: "26vw",
+          width: "100%"
+        }}
+      >
+        <img
+          style={{
+            width: "30vw"
+          }}
+          src="/text-img.webp"
+          alt="Goonies"
+        />
+      </Plx>
+      <div
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          zIndex: 200,
+          paddingTop: "56%",
+          height: "400vh",
+          width: "100%"
+        }}
+      >
+        <div
+          style={{
+            background: "#000",
+            height: "100%"
+          }}
+        ></div>
+      </div>
     </div>
+
     // <>
     //   <div
     //     className={backgroundImage}
