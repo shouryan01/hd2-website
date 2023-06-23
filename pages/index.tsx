f337f7ee63a90e4ffb6a43ddf9845cc4a2b0a21e
Shouryan Nikam <snnikam@umich.edu>
2023-06-23 19:19:15 -0400

    fix horizontal vertical issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import "react-horizontal-vertical/rhv.css";
+
 import About from "@/components/About";
 import Contact from "./contact";
 import Faq from "@/components/Faq";
@@ -11,6 +13,33 @@ import Tracks from '@/components/Tracks';
 
 export default function Home() {
   return (
+    // <div>
+    //   <First_Container />
+    //   <div className="flex flex-col snap-y snap-mandatory h-screen overflow-scroll">
+    //     <div className="snap-center">
+    //       <About />
+    //     </div>
+    //     <div className="snap-center h-screen">
+    //       <Tracks />
+    //     </div>
+    //     <div className="snap-center h-screen">
+    //       <Prizes />
+    //     </div>
+    //     <div className="snap-top h-screen">
+    //       <Sponsors />
+    //     </div>
+    //     <div className="snap-center h-screen">
+    //       <Faq />
+    //     </div>
+    //     <div className="snap-center h-screen">
+    //       <Contact />
+    //     </div>
+    //     <div className="snap-center h-screen">
+    //       <OurTeam />
+    //     </div>
+    //   </div>
+    // </div>
+
     <Rhv>
       <div className="flex min-h-screen flex-col items-center main-container">
         <First_Container />
